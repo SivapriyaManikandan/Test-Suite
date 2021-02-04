593899f3cb2242a0fb383086485dc5cf5b84e822
SivapriyaManikandan <way2priyamail@gmail.com>
2021-02-04 11:28:38 +1100

    Initial commit

--- Package1/TC_login.py
+++ Package1/TC_login.py
@@ -1,1 +1,11 @@
+import unittest
 
+class LoginTest(unittest.TestCase):
+    def test_loginbyemail(self):
+        print("Login by email")
+        self.assertTrue(True)
+    def test_loginbyfb(self):
+        print("Login by FB")
+        self.assertTrue(True)
+if __name__=="__main__":
+    unittest.main()
--- Package1/Tc_signup.py
+++ Package1/Tc_signup.py
@@ -1,1 +1,11 @@
+import unittest
 
+class SignupTest(unittest.TestCase):
+    def test_signbyemail(self):
+        print("SignUp by email")
+        self.assertTrue(True)
+    def test_signbyfb(self):
+        print("Sign UP by FB")
+        self.assertTrue(True)
+if __name__=="__main__":
+    unittest.main()
